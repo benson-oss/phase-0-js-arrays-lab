c9a846a313632b0cb579b85418884e418bbac639
Your Name <your@email.com>
2026-06-19 21:28:09 +0300

    arrays

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,31 @@
 // Write your code here
+// Array of product names
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
 
+function logFirstProduct() {
+  console.log("Laptop");}
 
+
+console.log(logFirstProduct()); 
+
+function addProduct(txt){
+   return products.push(txt)
+}
+
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;   
+    return products;            
+  } else {
+    return "Invalid index";     
+  }
+}
+ 
+
+  function removeLastProduct() {
+  return products.pop(); 
+  }
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
